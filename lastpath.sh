48f177258010b3775c20bd443c4cf7fdeccc4ca8
raph1324 <49415004+raph1324@users.noreply.github.com>
2019-04-14 11:57:09 +0200

    added support for bash

--- lastpath.sh
+++ lastpath.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# bash configuration:
+# in your .bashrc file add "PROMPT_COMMAND="history -a"
+
 # zsh configuration: 
 # in your .zshrc file add "setopt INC_APPEND_HISTORY"
 
-# make sure the path matches the $HISTFILE variable in your .zshrc
-histfile=$"/home/raph1324/.histfile"
+# get current shell
+if [ "$SHELL" = "/bin/bash" ]; then
+		histfile=$"/home/USER/.bash_history"
+else
+		# make sure the path matches the $HISTFILE variable in your .zshrc
+		histfile=$"/home/USER/.histfile"
+fi
 
 # wait for the user to lift their finger off the $mod key...
 sleep 0.2
